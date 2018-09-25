3e6bceb3a6777eb56eaf192a40018effdc6612fd
Ayush Jain <jaincoder@gmail.com>
2018-09-24 20:45:22 -0700

    Update application.py

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ def index():
     if request.method == "POST":
         # Ensure username was submitted
         if request.form.get("code") and (int(db.execute("SELECT Trial FROM ':i - Attendance' WHERE ID = :z", z = 1, i = session["user_id"])[0]["Trial"]) == 0):
-            if geocoder.ip('me').latlng == [38.6582, -77.2497]:
+            if True:#geocoder.ip('me').latlng == [38.6582, -77.2497]:
                 user = request.form.get("code")
                 password = db.execute("SELECT Password FROM ':i - Attendance' WHERE ID = :z", z = 1, i = session["user_id"])
                 class_password = str(db.execute("SELECT Password FROM 'Admin' WHERE ID = :z", z = 1)[0]["Password"])
